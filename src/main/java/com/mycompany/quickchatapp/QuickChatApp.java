58fb0f6b4c73eedfa77cf6113807cdc76bfc2f9b
Ezinhle <Ezinhle@EzinhlesPC.lan>
2026-04-12 20:14:11 +0200

    Commit 1: initialise and  setup project structure and scanner imports,package, import, and class skeleton.

---
+++ src/main/java/com/mycompany/quickchatapp/QuickChatApp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quickchatapp;
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author Ezinhle
+ */
+public class QuickChatApp {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
